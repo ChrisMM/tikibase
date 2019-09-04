8f784a32aebd96414b766d8ebc154abebcf4be09
Kevin Goslar <kevin.goslar@gmail.com>
2019-09-04 09:29:58 -0400

    Simple file content check

--- cuke_test.go
+++ cuke_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/DATA-DOG/godog"
 	"github.com/DATA-DOG/godog/gherkin"
@@ -26,7 +29,7 @@ func (w *workspaceFeature) createWorkspace(arg interface{}) {
 }
 
 func (w *workspaceFeature) containsFileWithContent(filename string, content *gherkin.DocString) error {
-	return ioutil.WriteFile(filename, []byte(content.Content), 0644)
+	return ioutil.WriteFile(path.Join(w.root, filename), []byte(content.Content), 0644)
 }
 
 func (w *workspaceFeature) runMentions() error {
@@ -34,7 +37,13 @@ func (w *workspaceFeature) runMentions() error {
 }
 
 func (w *workspaceFeature) shouldContainFileWithContent(filename string, content *gherkin.DocString) error {
-	return godog.ErrPending
+	data, err := ioutil.ReadFile(path.Join(w.root, filename))
+	exit.IfWrapf(err, "Cannot find file '%s' in workspace", filename)
+	text := string(data)
+	if strings.Compare(text, content.Content) != 0 {
+		return fmt.Errorf("text is different:\n\nEXPECTED:\n%s\n\nACTUAL:\n%s", content.Content, text)
+	}
+	return nil
 }
 
 //nolint:deadcode,unused
