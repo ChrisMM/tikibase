b2d7e1cb6d721c3c488dceb803c782d41a231121
Kevin Goslar <kevin.goslar@gmail.com>
2019-09-18 20:16:50 -0500

    Make cukes green

--- mentions/render_mentions_section.go
+++ mentions/render_mentions_section.go
@@ -9,9 +9,9 @@ import (
 // RenderMentionsSection provides a domain.Section containing the given mentions of a document.
 func RenderMentionsSection(links domain.TikiLinkCollection, doc *domain.Document) domain.Section {
 	builder := domain.NewSectionBuilder("### mentions\n\n", doc)
-	for _, link := range links {
-		sourceDoc := link.SourceSection().Document()
-		builder.AddLine(fmt.Sprintf("- [%s](%s)\n", sourceDoc.TitleSection().Title(), link.SourceSection().URL()))
+	for i := range links {
+		sourceDoc := links[i].SourceSection().Document()
+		builder.AddLine(fmt.Sprintf("- [%s](%s)\n", sourceDoc.TitleSection().Title(), links[i].SourceSection().URL()))
 	}
 	return builder.Section()
 }
