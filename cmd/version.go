81b6e88467c491e5bade98aae58ed6dec947854b
Kevin Goslar <kevin.goslar@gmail.com>
2020-03-18 05:02:09 -0500

    Version command (#105)

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+//nolint:gochecknoglobals
+// versionCmd defines the "version" command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Displays the TikiBase Tools version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("TikiBase Tools version 0.0.0")
+	},
+}
+
+//nolint:gochecknoinits
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
